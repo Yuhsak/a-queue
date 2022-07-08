8c14fed937af41582b618302f3f9e2312167414d
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2022-07-08 21:13:47 +0900

    Update deps, use swc for tests instead of ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/tests"
-  ],
-  "testMatch": [
-    "**/tests/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/tests/**/*.(ts|tsx|js|jsx)'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      '@swc/jest',
+      {
+        module: {
+          type: 'commonjs',
+        },
+        sourceMaps: true,
+      },
+    ],
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  verbose: true,
 }
